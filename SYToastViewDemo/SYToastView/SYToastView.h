adfcadcbc2c354fcd20bcb6ff5c2ee7117bcc589
紫竹 <hankangmac@hankangMACdeMac-mini.local>
2017-07-14 17:01:52 +0800

    Initial Commit

---
+++ SYToastViewDemo/SYToastView/SYToastView.h
@@ -0,0 +1,34 @@
+//
+//  SYToastView.h
+//  SYToastView
+//
+//  Created by hankangMAC on 2017/7/14.
+//  Copyright © 2017年 陈淑洋. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+@interface SYToastView : NSObject
+
+/***  显示导航栏提醒  ***/
+-(void)showNaviString:(NSString*)string;
+
+/*** 导航点击事件 ***/
+-(void)addTarget:(id)target action:(SEL)action;
+
+/*** 底部toast 默认时间显示为3s ***/
++(void)showToastString:(NSString *)string;
+
+/*** 底部toast  自定义显示时间***/
++ (void)showToastString:(NSString *)string showTime:(float)showTime;
+
+@end
+
+@interface ToastView : UILabel
++(ToastView*) sharedToastView;
+@end
+
+
+@interface CSYToastLabel : UILabel
++(CSYToastLabel*) sharedCSYToastLabel;
+@end
---
+++ SYToastViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SYToastViewDemo
+//
+//  Created by 紫竹 on 2017/7/14.
+//  Copyright © 2017年 CSY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
